3c3a25ba245ec5c9e61f80b79bcd57f7041afaa4
Duta-Sebastian <sebiduta2004@gmail.com>
2024-11-04 00:52:46 +0200

    Trying to fix windows installation of Postgres, removed sample test.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,6 @@
 #include <gtest/gtest.h>
 
-// Add your test cases here
-TEST(SampleTest, AssertionTrue) {
-    ASSERT_TRUE(true);
-}
+/// TODO Maybe add a script that initializes a brand new docker container and runs the tests in that container
 
 // This is where the test suite runs
 int main(int argc, char **argv) {
