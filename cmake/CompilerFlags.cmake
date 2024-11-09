6d10b3393a748f7ca1a885a00af29b284ce5e1f6
Duta-Sebastian <sebiduta2004@gmail.com>
2024-11-10 00:51:37 +0200

    Fixed apple clang

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -16,7 +16,7 @@ message("Compiler: ${CMAKE_CXX_COMPILER_ID} version ${CMAKE_CXX_COMPILER_VERSION
 if (MSVC)
     target_compile_options(${PROJECT_NAME} PRIVATE /W4 /permissive- /wd4244 /wd4267 /wd4996 /external:anglebrackets /external:W0 /utf-8 /MP)
 else ()
-    target_compile_options(${PROJECT_NAME} PRIVATE -Wall -Wextra -pedantic)
+    target_compile_options(${PROJECT_NAME} PRIVATE -Wall -Wextra -pedantic -Wno-pre-c++2b-compat)
 endif ()
 
 ###############################################################################
